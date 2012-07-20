09c33b2708a1982aa1fbd1fbb5b60e78c0477f36
Philipp Gayret <philipp@skillable.eu>
2012-07-20 11:10:52 +0200

    Accepted videos servlet

--- src/khan/videos/servlets/TribunalServlet.java
+++ src/khan/videos/servlets/TribunalServlet.java
@@ -9,19 +9,26 @@ import javax.servlet.http.HttpServletResponse;
 
 import khan.videos.DAO;
 import khan.videos.models.Video;
+import khan.videos.models.Video.Status;
 import khan.videos.servlets.templates.Templater;
 
 @SuppressWarnings("serial")
 public class TribunalServlet extends HttpServlet {
 
+	public static final String requestTribunalVoting = "/tribunal";
+	public static final String requestTribunalAccepted = "/accepted";
+
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		DAO dao = new DAO();
-		List<Video> videos = dao.ofy().query(Video.class).order("submitted").list();
-		Templater templater = new Templater();
-		if (req.getParameter("message") != null) {
-			templater.put("message", req.getParameter("message"));
+		String requestTribunal = req.getServletPath();
+		List<Video> videos = null;
+		if (requestTribunalVoting.equals(requestTribunal)) {
+			videos = dao.ofy().query(Video.class).filter("status", Status.Voting).order("submitted").list();
+		} else if (requestTribunalAccepted.equals(requestTribunal)) {
+			videos = dao.ofy().query(Video.class).filter("status", Status.Accepted).order("submitted").list();
 		}
+		Templater templater = new Templater();
 		templater.put("videos", videos);
 		templater.render("tribunal.html", req, resp);
 	}
