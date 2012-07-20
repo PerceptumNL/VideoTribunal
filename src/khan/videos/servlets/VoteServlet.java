f63e618525c5b6a825f2fa1157bfc355832e442e
Philipp Gayret <philipp@skillable.eu>
2012-07-20 15:05:48 +0200

    Replace cronjob with vote handler

--- src/khan/videos/servlets/VoteServlet.java
+++ src/khan/videos/servlets/VoteServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import khan.videos.DAO;
 import khan.videos.models.AppUser;
 import khan.videos.models.Video;
+import khan.videos.models.Video.Status;
 import khan.videos.models.Vote;
 import khan.videos.servlets.login.BaseUserServlet;
 import khan.videos.servlets.templates.Templater;
@@ -19,6 +20,10 @@ import com.googlecode.objectify.NotFoundException;
 
 @SuppressWarnings("serial")
 public class VoteServlet extends BaseUserServlet {
+
+	public static final Integer REQUIRED_VOTES = 4;
+	public static final Double PERCENTAGE_ACCEPT = 0.75;
+	public static final Double PERCENTAGE_DENY = 0.6;
 
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp, AppUser user) throws IOException {
@@ -42,7 +47,28 @@ public class VoteServlet extends BaseUserServlet {
 		}
 	}
 
-	// TODO: Topics, Exercise List
+	// TODO: Test
+	public static void runTribunal(Video video, List<Vote> votes) {
+		if (votes.size() >= REQUIRED_VOTES) {
+			DAO dao = new DAO();
+			Double voteTotal = new Double(0);
+			for (Vote vote : votes) {
+				voteTotal += vote.getWeight();
+			}
+			Double votePercentage = ((voteTotal + votes.size()) / 2) / votes.size();
+			if (votePercentage >= VoteServlet.PERCENTAGE_ACCEPT) {
+				// Functioning
+				video.setStatus(Status.Accepted);
+				dao.ofy().put(video);
+			} else if (votePercentage <= VoteServlet.PERCENTAGE_DENY) {
+				// Error (?)
+				video.setStatus(Status.Denied);
+				dao.ofy().put(video);
+			}
+			System.out.println(votePercentage);
+		}
+	}
+
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse resp, AppUser user) throws IOException {
 		String youtubeId = req.getParameter("youtubeId");
@@ -58,6 +84,9 @@ public class VoteServlet extends BaseUserServlet {
 					dao.ofy().put(new Vote(user, video, null, new ArrayList<String>(), accepted, comment));
 					req.getSession().setAttribute("message", "Je reactie op de video is opgeslagen, bedankt!");
 					resp.sendRedirect("/tribunal");
+					List<Vote> votes = dao.ofy().query(Vote.class)
+							.filter("video", new Key<Video>(Video.class, video.getYoutubeId())).list();
+					VoteServlet.runTribunal(video, votes);
 				} else {
 					req.getSession().setAttribute("message", "Je hebt al een reactie op deze video geplaatst.");
 					resp.sendRedirect("/tribunal");
@@ -69,5 +98,4 @@ public class VoteServlet extends BaseUserServlet {
 			resp.sendRedirect("/tribunal");
 		}
 	}
-
 }
